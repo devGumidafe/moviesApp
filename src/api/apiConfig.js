5a6ea31e0e1216e7ce05beb48602e7b8df3653fa
gumidev <devgumidafe@gmail.com>
2021-11-17 00:04:14 +0000

    config api

---
+++ src/api/apiConfig.js
@@ -0,0 +1,6 @@
+export const apiConfig = {
+  baseUrl: "https://api.themoviedb.org/3/",
+  apiKey: "9838bc8f800d35bea3f05178c6b89423",
+  originalImage: (imgPath) => `https://image.tmdb.org/t/p/original${imgPath}`,
+  w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500${imgPath}`,
+};
