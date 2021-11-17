ed70ead6d734a569cf25d653422e6a13e2d086da
gumidev <devgumidafe@gmail.com>
2021-11-17 16:28:52 +0000

    create and style button

---
+++ src/components/buttons/Button.jsx
@@ -0,0 +1,26 @@
+import PropTypes from "prop-types";
+
+export const Button = (props) => {
+  return (
+    <button
+      className={`btn ${props.className}`}
+      onClick={props.onClick ? () => props.onClick() : null}
+    >
+      {props.children}
+    </button>
+  );
+};
+
+const OutLineButton = (props) => {
+  return (
+    <Button
+      className={`btn-outline ${props.className}`}
+      onClick={props.onClick ? () => props.onClick() : null}
+    >
+      {props.children}
+    </Button>
+  );
+};
+Button.propTypes = {
+  onClick: PropTypes.func.isRequired,
+};
