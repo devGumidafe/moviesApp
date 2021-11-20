d50e5ff022afa13aa4c7727b593466737f849329
gumidev <devgumidafe@gmail.com>
2021-11-20 22:52:01 +0000

    fixed routes"

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import { AppRouter } from "./routers/AppRouter";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
+import { HashRouter } from "react-router-dom";
+import { Header } from "./components/header/Header";
+import { Footer } from "./components/footer/Footer";
 import "swiper/swiper.min.css";
 import "./assets/boxicons-2.0.7/css/boxicons.min.css";
 import "./App.scss";
 
 export const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/*" element={<AppRouter />} />
-      </Routes>
-    </Router>
+    <HashRouter>
+      <Header />
+      <AppRouter />
+      <Footer />
+    </HashRouter>
   );
 };
--- src/routers/AppRouter.jsx
+++ src/routers/AppRouter.jsx
@@ -2,22 +2,17 @@ import { Routes, Route } from "react-router-dom";
 import { Home } from "../pages/Home";
 import { Detail } from "../pages/detail/Detail";
 import { Catalog } from "../pages/Catalog";
-import { Footer } from "../components/footer/Footer";
-import { Header } from "../components/header/Header";
+import { NotFound } from "../pages/NotFound";
 
 export const AppRouter = () => {
   return (
-    <>
-      <Header />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path=":category/search/:keyword" element={<Catalog />} />
+      <Route path=":category/:id" element={<Detail />} />
+      <Route path=":category" element={<Catalog />} />
 
-      <Routes>
-        <Route path="/:category/search/:keyword" element={<Catalog />} />
-        <Route path="/:category/:id" element={<Detail />} />
-        <Route path="/:category" element={<Catalog />} />
-        <Route path="/*" element={<Home />} />
-      </Routes>
-
-      <Footer />
-    </>
+      <Route path="*" element={<NotFound />} />
+    </Routes>
   );
 };
